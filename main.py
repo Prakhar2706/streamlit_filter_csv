45ca1d9c4eb4a1ef6e1e8aacd0f2f2d1a623ecb4
Prakhar <76550694+Prakhar2706@users.noreply.github.com>
2024-09-29 10:51:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,22 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 st.title("Simple Data Dashboard")
-upload_file = st.file_uploader("Upload an Excel file", type=["xlsx", "xls"])
+upload_file = st.file_uploader("Upload a CSV file", type="csv")
 if upload_file is not None:
-    df = pd.read_excel(upload_file)
+    # st.write("File Uploaded Successfully...")
+    df = pd.read_csv(upload_file)
     st.subheader("Data Preview")
     st.write(df.head())
-    
+    # st.dataframe(df)
     st.subheader("Data Summary")
     st.write(df.describe())
 
     st.subheader("Filter Data")
     columns = df.columns.tolist()
+    # selected_columns = st.multiselect("Select Columns to Filter by", columns)
+    # if selected_columns:
+    #     filtered_df = df[selected_columns]
+    #     st.write(filtered_df)
     selected_column = st.selectbox("Select Columns to Filter by", columns)
     unique_value = df[selected_column].unique()
     selected_value = st.selectbox("Select Values to Filter", unique_value)
@@ -26,12 +31,7 @@ if upload_file is not None:
     y_column = st.selectbox("Select y_axis column to plot", columns)
     
     if st.button("Generate Plot"):
-        fig, ax = plt.subplots()
-        ax.plot(filtered_df[x_column], filtered_df[y_column], marker='o')
-        ax.set_xlabel(x_column)
-        ax.set_ylabel(y_column)
-        ax.set_title(f"{y_column} vs {x_column}")
-        st.pyplot(fig)
+        st.line_chart(filtered_df.set_index(x_column)[y_column])
 
 else:
-    st.warning("Please upload an Excel file.")
+    st.warning("Please upload a CSV file.")
